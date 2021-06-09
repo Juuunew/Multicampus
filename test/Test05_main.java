e69b894cb624b5152a7c68fda91e2dfbedcf616b
Juuunew <74216712+Juuunew@users.noreply.github.com>
2021-06-09 22:41:18 +0900

    Add files via upload

---
+++ test/Test05_main.java
@@ -0,0 +1,15 @@
+package test;
+
+import ui.Stock;
+
+public class Test05_main {
+
+	public static void main(String[] args) {
+		// stock s 객체를 메모리에 띄움
+		Stock s = new Stock();
+		
+		// stock class onCreate 불러오기
+		s.onCreate();
+	}
+
+}
